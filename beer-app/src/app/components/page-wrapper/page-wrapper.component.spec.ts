a9d34832c0157b857d1f6d8c21d9e8df24be931c
Ted Williams <64963963+Ted-Williams@users.noreply.github.com>
2021-10-28 19:19:56 +0100

    Create page-wrapper.component.spec.ts

---
+++ beer-app/src/app/components/page-wrapper/page-wrapper.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageWrapperComponent } from './page-wrapper.component';
+
+describe('PageWrapperComponent', () => {
+  let component: PageWrapperComponent;
+  let fixture: ComponentFixture<PageWrapperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageWrapperComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageWrapperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
