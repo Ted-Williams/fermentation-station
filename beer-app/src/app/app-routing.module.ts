88e1c1367b7261e62d57e4302997364dd32a5e07
Ted Williams <64963963+Ted-Williams@users.noreply.github.com>
2021-10-28 19:16:28 +0100

    Fixed routing

--- beer-app/src/app/app-routing.module.ts
+++ beer-app/src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { BeerPageComponent } from './components/beer-page/beer-page.component';
 import { FoodPageComponent } from './components/food-page/food-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { PageWrapperComponent } from './components/page-wrapper/page-wrapper.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'', component:AppComponent, children:[
-    {path:'', component:HomePageComponent},
+
+  {path:'', component:PageWrapperComponent, children:[
+    {path:'', redirectTo:'/home', pathMatch:'full'},
+    {path:'login', component:LoginComponent},
     {path:'home', component:HomePageComponent},
     {path:'beer', component:BeerPageComponent},
-    {path:'food', component:FoodPageComponent}
-  ]}
+    {path:'food', component:FoodPageComponent},
+
+  ]},
+  {path:'**', redirectTo:'/home', pathMatch:'full'},
 ];
 
 @NgModule({
